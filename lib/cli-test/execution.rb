9038b1fd9c81844de261e0331662ad733532f08a
Tim Hoiberg <tim.hoiberg@gmail.com>
2015-12-31 17:25:30 +1000

    Modified documentation and fixed up indentation in files

--- lib/cli-test/execution.rb
+++ lib/cli-test/execution.rb
@@ -2,30 +2,38 @@
 module CliTest
   ##
   # Encapsulates the execution results from the command
-   class Execution
-      extend Forwardable
-      attr_reader :stdout, :stderr, :status
+  class Execution
+    extend Forwardable
 
-      ##
-      # creates an object of type CliTest::Execution
-      # @param [String] stdout the stdout of the execution
-      # @param [String] stderr the stderr of the execution
-      # @param [Process::Status] status the status of the execution
-      # @return [CliTest::Execution] the execution object
-      def initialize(stdout, stderr, status)
-          @stdout = stdout
-          @stderr = stderr
-          @status = status
-       end
+    ##
+    # @!attribute [r] stdout
+    #   @return [String] the stdout from the execution
+    # @!attribute [r] stderr
+    #   @return [String] the stderr from the execution
+    # @!attribute [r] stats
+    #   @return [Process::Status] the status of the execution
+    attr_reader :stdout, :stderr, :status
 
-      ##
-      # returns true if the status code for the execution is
-      # non zero
-      # @return [Boolean] whether the execution was successful
-      def successful?
-        @status.success?
-      end
+    ##
+    # creates an object of type CliTest::Execution
+    # @param [String] stdout the stdout of the execution
+    # @param [String] stderr the stderr of the execution
+    # @param [Process::Status] status the status of the execution
+    # @return [CliTest::Execution] the execution object
+    def initialize(stdout, stderr, status)
+      @stdout = stdout
+      @stderr = stderr
+      @status = status
+    end
 
-      def_delegator :@status, :exitstatus
-   end
+    ##
+    # returns true if the status code for the execution is
+    # non zero
+    # @return [Boolean] whether the execution was successful
+    def successful?
+      @status.success?
+    end
+
+    def_delegator :@status, :exitstatus
+  end
 end
